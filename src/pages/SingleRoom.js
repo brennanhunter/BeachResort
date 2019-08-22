d520e2daf627eed93cc6f6d4282b3b23967f59d3
Hunter <brennanhunter58@gmail.com>
2019-08-22 13:43:01 -0600

    SingleRoom Image completed

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import deafultBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
+import {RoomContext} from '../context'
+import StyledHero from '../components/StyledHero'
+
 
 class SingleRoom extends Component {
+    constructor(props) {
+        super(props)
+        // console.log(this.props)
+        this.state = {
+            slug:this.props.match.params.slug,
+            deafultBcg
+        }
+    }
+    static contextType = RoomContext
+    // componentDidMount() {
+        
+    // }
     render() {
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.slug);
+        if(!room) {
+            return (
+                <div className="error">
+                    <h3>No room found...</h3>
+                    <Link to= '/rooms' className="btn-primary">Back to Rooms</Link>
+                </div>
+            );
+        }
+        const {name, description, capacity, size, price, extras, breakfast, pets, images} = room
         return (
-            <div>
-                Hello from Single Room Page
-            </div>
+            <StyledHero img={images[0]}>
+                <Banner title={`${name} room`}>
+                    <Link to='/rooms' className='btn-primary'>Back to Rooms</Link>
+                </Banner>
+            </StyledHero>
         );
     }
 }
