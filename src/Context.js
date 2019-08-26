9e2ae7a38a79358f670e2106ed41cab9e273ea65
Hunter <brennanhunter58@gmail.com>
2019-08-26 16:32:51 -0600

    fixed file

--- src/Context.js
+++ src/Context.js
@@ -44,16 +44,16 @@ class RoomProvider extends Component {
 
     componentDidMount() {
         this.getData()
-        // let rooms = this.formatData(items);
-        // let featuredRooms = rooms.filter(room => room.featured === true);
-        // let maxPrice = Math.max(...rooms.map(item =>
-        //     item.price));
-        // let maxSize = Math.max(...rooms.map(item =>
-        //     item.size));
+        let rooms = this.formatData(items);
+        let featuredRooms = rooms.filter(room => room.featured === true);
+        let maxPrice = Math.max(...rooms.map(item =>
+            item.price));
+        let maxSize = Math.max(...rooms.map(item =>
+            item.size));
 
-        // this.setState({
-        //     rooms, featuredRooms, sortedRooms: rooms, loading: false, price: maxPrice, maxPrice, maxSize
-        // })
+        this.setState({
+            rooms, featuredRooms, sortedRooms: rooms, loading: false, price: maxPrice, maxPrice, maxSize
+        })
     }
 
     formatData(items) {
