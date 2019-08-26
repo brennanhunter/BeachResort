b1f8c9ed05bc94fb86547605699829b8f5ae78ae
Hunter <brennanhunter58@gmail.com>
2019-08-26 14:40:03 -0600

    pre contentful

--- src/Context.js
+++ src/Context.js
@@ -16,7 +16,8 @@ class RoomProvider extends Component {
         maxPrice: 0,
         maxSize: 0,
         breakfast: false,
-        pets: false
+        pets: false,
+        minSize: 0
     };
 
     componentDidMount() {
@@ -48,11 +49,67 @@ class RoomProvider extends Component {
         return room
     }
 
+handleChange = event => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = event.target.name;
 
+    this.setState(
+        {
+          [name]: value
+        },
+        this.filterRooms
+      );
+}
+
+filterRooms = () => {
+    let {
+        rooms, type, capacity, price, minSize, maxSize, breakfast, pets
+    } = this.state
+// all the rooms
+    let tempRooms = [...rooms];
+
+//transform values
+    capacity = parseInt(capacity)
+    price = parseInt(price)
+
+//filter by type
+
+    if (type !== 'all') {
+        tempRooms = tempRooms.filter(room => room.type === type)
+    }
+
+//filter by capacity
+    if(capacity !== 1) {
+        tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+    }
+
+// filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price)
+
+//filter by size
+    tempRooms= tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+
+//filter by breakfast
+    if(breakfast) {
+        tempRooms = tempRooms.filter(room => room.breakfast === true)
+    }
+
+//filter by pets
+if(pets) {
+    tempRooms = tempRooms.filter(room => room.pets === true)
+}
+
+
+//change state
+    this.setState({
+        sortedRooms:tempRooms
+    })
+}
 
 
     render() {
-        return <RoomContext.Provider value={{...this.state, getRoom:this.getRoom}}>
+        return <RoomContext.Provider value={{...this.state, getRoom:this.getRoom, handleChange:this.handleChange}}>
             {this.props.children}
         </RoomContext.Provider>;
         }
